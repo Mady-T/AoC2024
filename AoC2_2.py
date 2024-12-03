8c81ff72c17f80703ade7bbfd9569cf866697169
Mady <adithya5463sajith@gmail.com>
2024-12-03 19:41:22 +0530

    Solve day 2 part 2

--- AoC2_2.py
+++ AoC2_2.py
@@ -4,57 +4,49 @@ inp = pd.read_csv('inputs/day2inp1', sep='\s+', header=None, names = range(8))
 
 safe_count = 0
 for report_num in range(len(inp)):
-    report = list(inp.iloc[report_num,:])
-    n = 0
-    asc = False
-    desc = False
-    safe = True
-    safety_net = True
-    while n < len(report) and report[n]:
-        if n == 0:
-            n+=1
-            continue
-        if abs(report[n] - report[n-1]) > 3:
-                if safety_net:
-                    safety_net = False
-                    report.pop(n-1)
-                    n-=1
-                    break
-                else:
+    report_ = list(inp.iloc[report_num,:])
+    reportlist = []
+    for i in range(len(report_)):
+        rep = report_.copy()
+        rep.pop(i)
+        reportlist.append(rep)
+    anysafe = False
+    for report in reportlist:
+        n = 0
+        asc = False
+        desc = False
+        safe = True
+        while n < len(report) and report[n]:
+            if n == 0:
+                n+=1
+                continue
+            if abs(report[n] - report[n-1]) > 3:
                     safe = False
                     break
-        if not asc and not desc:
-            if report[n] > report[n-1]:
-                asc = True
-            elif report[n] < report[n-1]:
-                desc= True
+            if not asc and not desc:
+                if report[n] > report[n-1]:
+                    asc = True
+                elif report[n] < report[n-1]:
+                    desc= True
+                else:
+                    safe=False
+                    break
             else:
-                safe=False
-                break
-        else:
-            if asc:
-                if report[n] <= report[n-1]:
-                    if safety_net:
-                        safety_net = False
-                        report.pop(n-1)
-                        n-=1
-                        break
-                    else:
+                if asc:
+                    if report[n] <= report[n-1]:
                         safe = False
                         break
-            if desc:
-                if report[n] >= report[n-1]:
-                    if safety_net:
-                        safety_net = False
-                        report.pop(n-1)
-                        n-=1
-                        break
-                    else:
+                if desc:
+                    if report[n] >= report[n-1]:
                         safe = False
                         break
-        n+=1
-    print(report, safe)
-    if safe:
+            n+=1
+        # print(report, safe)
+        if safe:
+            anysafe = True
+            break
+    # print(report, anysafe)
+    if anysafe:
         safe_count+= 1
 
 print(safe_count)
